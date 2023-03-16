b46788a9834a65f14e600c59600dc3272fc9211d
milkyfuckinway <0-0_0@bk.ru>
2023-03-16 21:14:21 +0300

    fix

--- source/js/modules/map/map.js
+++ source/js/modules/map/map.js
@@ -1,5 +1,9 @@
 const destination = [48.69692660883092, 44.50245328354335];
 const map = document.querySelector('#map');
+const link = 'https://api-maps.yandex.ru/2.1/?apikey=7c70a677-2343-4514-aa84-bc38e108561a&lang=ru_RU';
+const ymapsScript = document.createElement('script');
+ymapsScript.src = link;
+
 let executed = false;
 
 window.addEventListener('scroll', activateMapOnScroll);
@@ -21,28 +25,22 @@ const init = () => {
 };
 
 const createScriptLink = () => {
-  const link =
-    'https://api-maps.yandex.ru/2.1/?apikey=7c70a677-2343-4514-aa84-bc38e108561a&lang=ru_RU';
-  const scriptLink = document.createElement('script');
-  scriptLink.src = link;
-  document.body.appendChild(scriptLink);
+  document.body.appendChild(ymapsScript);
 };
 
 const initMap = () => {
-  const timeout = 2000;
-  setTimeout(() => {
-    ymaps.ready(init);
-  }, timeout);
+  ymaps.ready(init);
 };
 
 function activateMapOnScroll() {
-  if (
-    map.getBoundingClientRect().top - window.innerHeight < 2000 &&
-    !executed
-  ) {
+  if (map.getBoundingClientRect().top - window.innerHeight < 2000 && !executed) {
     executed = true;
     createScriptLink();
-    initMap();
+    ymapsScript.addEventListener('load', () => {
+      if (typeof ymaps !== 'undefined' && ymaps !== null) {
+        initMap();
+      }
+    });
     window.removeEventListener('scroll', activateMapOnScroll);
   }
 }
